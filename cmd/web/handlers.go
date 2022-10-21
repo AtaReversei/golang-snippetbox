aa25e8c138f924ed8cafad407d8b4b14eb9cd80a
atareversei <faraji.ata92@gmail.com>
2022-10-21 15:32:57 +0330

    [ADD] templates

---
+++ cmd/web/handlers.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error. Could not parse html.", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error. Could not execute html.", 500)
+	}
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "showing snippet with the id of %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method not allowed..."))
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Write([]byte("creating snippets"))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,39 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from the snippet box!"))
-}
-
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "showing snippet with the id of %d", id)
-}
-
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		//w.WriteHeader(405)
-		//w.Write([]byte("Method not allowed..."))
-		http.Error(w, "Method not allowed", 405)
-		return
-	}
-	w.Write([]byte("creating snippets"))
-}
 
 func main() {
 	mux := http.NewServeMux()
