4084d9b6a7aeef7446ebe41453c399c1c7f3f3c2
atareversei <faraji.ata92@gmail.com>
2022-10-21 12:24:16 +0330

    [ADD] handlers and method types

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from the snippet box!"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "showing snippet with the id of %d", id)
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		//w.WriteHeader(405)
+		//w.Write([]byte("Method not allowed..."))
+		http.Error(w, "Method not allowed", 405)
+		return
+	}
+	w.Write([]byte("creating snippets"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("starting server on port :4000")
 	err := http.ListenAndServe(":4000", mux)
